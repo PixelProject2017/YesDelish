9c0e366b7a73b364a6075c863f08e8d0ea0441a4
KHA HOANG Y NGUYEN <ykha@cs.washington.edu>
2017-02-14 17:51:05 -0800

    pop up window for registration

--- main_web/webapp/views.py
+++ main_web/webapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 def index(request):
 	return render(request, 'web_content/index.html')
+
+def sign_up(request):
+    return render(request, 'web_content/hello.html')
